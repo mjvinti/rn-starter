d10d46850afec9289a9d01ef960d02c667f13deb
Mario Vinti <mjvinti@gmail.com>
2022-10-26 16:51:33 -0400

    passing callbacks to children

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onDecrease, onIncrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} />
-      <Button title={`Decrease ${color}`} />
+      <Button title={`Increase ${color}`} onPress={onIncrease} />
+      <Button title={`Decrease ${color}`} onPress={onDecrease} />
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Blue" />
+      <ColorCounter
+        color="Red"
+        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + 1)}
+      />
       <ColorCounter color="Green" />
+      <ColorCounter color="Blue" />
     </View>
   );
 };
