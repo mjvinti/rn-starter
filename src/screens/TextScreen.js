4533875416ee58d7fd738f1a1d1f91f45c8825a3
Mario Vinti <mjvinti@gmail.com>
2022-10-31 16:08:22 -0400

    showing text input

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>TextScreen</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 15,
+  },
+});
 
 export default TextScreen;
